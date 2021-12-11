3c7026a0c75b5f9b09b0848e979b241c771351a5
bqx619 <bqx619@gmail.com>
2021-12-11 18:33:06 +0800

    feat: add mainnet setting

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,94 +1,119 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt, log } from "@graphprotocol/graph-ts"
 import {
-  PeopleLandV1,
-  Approval,
-  ApprovalForAll,
   GiveTo,
   Mint,
-  OwnershipTransferred,
+  PeopleLandV1,
   SetSlogan,
-  Transfer
+  Transfer,
 } from "../generated/PeopleLandV1/PeopleLandV1"
-import { ExampleEntity } from "../generated/schema"
+import { TokenInfo, People } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+let ZeroAddress = Address.fromString("0x0000000000000000000000000000000000000000")
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.PRICE(...)
-  // - contract.SIGN_MESSAGE_ADDRESS(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getCoordinates(...)
-  // - contract.getCoordinatesStrings(...)
-  // - contract.getInviteParams(...)
-  // - contract.getMintLands(...)
-  // - contract.getMintedAndInvitedList(...)
-  // - contract.getNeighborsParams(...)
-  // - contract.getTokenId(...)
-  // - contract.givedLand(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.isBuilder(...)
-  // - contract.isPeople(...)
-  // - contract.land(...)
-  // - contract.mintLandCount(...)
-  // - contract.mintSelfSwitch(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.packedXY(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenSVGAddress(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
+function getLandId(x: BigInt, y: BigInt): string {
+  return x.toString() + "-" + y.toString()
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+function getPeople(address: Address): People {
+  let people = People.load(address.toHexString())
+  if (people == null) {
+    people = new People(address.toHexString())
+    people.save()
+  }
+  return people as People
+}
 
-export function handleGiveTo(event: GiveTo): void {}
+function removePeopleLand(people: People, landId: string): void {
+  if (people.lands.includes(landId)) {
+    people.lands.splice(people.lands.findIndex(item => item === landId), 1)
+    people.save()
+  }
+}
 
-export function handleMint(event: Mint): void {}
+function addPeopleLand(people: People, landId: string): void {
+  if (!people.lands.includes(landId)) {
+    people.lands.push(landId)
+    people.save()
+  }
+}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleMint(event: Mint): void {
 
-export function handleSetSlogan(event: SetSlogan): void {}
+  let x = event.params.x
+  let y = event.params.y
+  let _id = getLandId(x, y)
 
-export function handleTransfer(event: Transfer): void {}
+  let tokenInfo = TokenInfo.load(_id)
+  if (tokenInfo === null) {
+    tokenInfo = new TokenInfo(_id)
+    tokenInfo.x = x
+    tokenInfo.y = y
+    tokenInfo.minted = getPeople(event.params.mintedAddress).id
+    tokenInfo.owner = getPeople(ZeroAddress).id
+    tokenInfo.createdAtTimestamp = event.block.timestamp
+    tokenInfo.createdAtBlockNumber = event.block.number
+  }
+  tokenInfo.save()
+}
+
+export function handleGiveTo(event: GiveTo): void {
+
+  let contract = PeopleLandV1.bind(event.address)
+
+  let x = event.params.x
+  let y = event.params.y
+  let _id = getLandId(x, y)
+
+  let tokenInfo = TokenInfo.load(_id)
+  let owner = getPeople(event.params.givedAddress)
+  tokenInfo.owner = owner.id
+  addPeopleLand(owner, _id)
+  
+  let tokenId = contract.getTokenId(x, y)
+  let tokenURI = contract.tokenURI(tokenId)
+  let land = contract.land(x, y)
+
+  tokenInfo.tokenId = tokenId
+  tokenInfo.tokenSvg = tokenURI
+  tokenInfo.slogan = land.slogan
+  tokenInfo.givedAtBlockNumber = event.block.number
+  tokenInfo.givedAtTimestamp = event.block.timestamp
+
+  let neighbors = contract.getNeighborsParams(x, y)
+  tokenInfo.neighbors = neighbors.filter((n) => n !== null)
+  tokenInfo.save()
+}
+
+export function handleTransfer(event: Transfer): void {
+  let tokenId = event.params.tokenId
+  let contract = PeopleLandV1.bind(event.address)
+  let coor = contract.getCoordinates(tokenId)
+  let x = coor.value0
+  let y = coor.value1
+  let _id = getLandId(x, y)
+
+  let tokenInfo = TokenInfo.load(_id)
+
+  if (tokenInfo === null) {
+    tokenInfo = new TokenInfo(_id)
+    tokenInfo.x = x
+    tokenInfo.y = y
+    tokenInfo.createdAtTimestamp = event.block.timestamp
+    tokenInfo.createdAtBlockNumber = event.block.number
+  }
+  let owner = getPeople(event.params.to)
+  tokenInfo.owner = owner.id
+  
+  tokenInfo.save()
+
+  let from = getPeople(event.params.from)
+  removePeopleLand(from, _id)
+  addPeopleLand(owner, _id)
+}
+
+export function handleSetSlogan(event: SetSlogan): void {
+  let _id = getLandId(event.params.x, event.params.y)
+  let tokenInfo = TokenInfo.load(_id)
+  tokenInfo.slogan = event.params.slogan
+  tokenInfo.save()
+}
